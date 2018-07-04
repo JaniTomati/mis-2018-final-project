c6c669d2cf292ac7acf36aa51d83c95b33a6ce46
JaniTomati <jana.puschmann@uni-weimar.de>
2018-07-04 15:02:12 +0200

    Printwriter does not work on main thread.

--- app/src/main/java/com/touchprojector/mis_2018_final_project/MainActivity.java
+++ app/src/main/java/com/touchprojector/mis_2018_final_project/MainActivity.java
@@ -11,6 +11,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -34,13 +35,13 @@ import java.net.UnknownHostException;
 public class MainActivity extends AppCompatActivity {
 
     private static final int MY_CAMERA_REQUEST_CODE = 100;
+    private static final String TOUCH_TAG = "TouchEvent";
 
     Context context;
 
     private boolean isConnected = false;
     private boolean mouseMoved = false;
     private Socket socket;
-    private TextView touchIn;
     private Button connectBtn;
     private Button scanBtn;
 
@@ -48,9 +49,14 @@ public class MainActivity extends AppCompatActivity {
     private ImageView image;
     private Bitmap bMap;
 
+    private float initialX;
+    private float initialY;
+    private float finalX;
+    private float finalY;
+
     private IntentIntegrator qrScan;
-    private String server_ip;
-    private int server_port;
+    private String server_ip = "192.168.0.32";
+    private int server_port = 8090;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 ConnectPhoneTask connectPhoneTask = new ConnectPhoneTask();
-                connectPhoneTask.execute(Constants.SERVER_IP); //try to connect to server in another thread
+                connectPhoneTask.execute(server_ip); //try to connect to server in another thread
             }
         });
 
@@ -147,12 +153,73 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * http://codetheory.in/android-ontouchevent-ontouchlistener-motionevent-to-detect-common-gestures/
+     */
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int action = event.getActionMasked();
+
+        switch (action) {
+
+            case MotionEvent.ACTION_DOWN:
+                initialX = event.getX();
+                initialY = event.getY();
+
+                Log.d(TOUCH_TAG, "Action was DOWN");
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                Log.d(TOUCH_TAG, "Action was MOVE");
+                break;
+
+            case MotionEvent.ACTION_UP:
+                finalX = event.getX();
+                finalY = event.getY();
+
+                if (socket.isConnected() && outData != null) {
+                    outData.println("MouseClick:" + finalX + ":" + finalY);
+                    Log.i("OutStream", "Sendet Daten " + finalX + " " + finalY);
+                }
+
+                Log.d(TOUCH_TAG, "Action was UP");
+
+                if (initialX < finalX) {
+                    Log.d(TOUCH_TAG, "Left to Right swipe performed");
+                }
+
+                if (initialX > finalX) {
+                    Log.d(TOUCH_TAG, "Right to Left swipe performed");
+                }
+
+                if (initialY < finalY) {
+                    Log.d(TOUCH_TAG, "Up to Down swipe performed");
+                }
+
+                if (initialY > finalY) {
+                    Log.d(TOUCH_TAG, "Down to Up swipe performed");
+                }
+
+                break;
+
+            case MotionEvent.ACTION_CANCEL:
+                Log.d(TOUCH_TAG,"Action was CANCEL");
+                break;
+
+            case MotionEvent.ACTION_OUTSIDE:
+                Log.d(TOUCH_TAG, "Movement occurred outside bounds of current screen element");
+                break;
+        }
+
+        return super.onTouchEvent(event);
+    }
+
 
     @Override
     public void onDestroy()
     {
         super.onDestroy();
-        if(isConnected && outData != null) {
+        if(socket.isConnected() /*&& outData != null*/) {
             try {
                 outData.println("exit"); //tell server to exit
                 socket.close(); //close socket
@@ -161,15 +228,34 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
-
+    
     public class ConnectPhoneTask extends AsyncTask<String,Void,Boolean> {
         @Override
         protected Boolean doInBackground(String... params) {
             boolean result = true;
             try {
                 InetAddress serverAddr = InetAddress.getByName(params[0]);
-                socket = new Socket(server_ip, server_port);//Open socket on server IP and port
+                socket = new Socket(server_ip, server_port); //Open socket on server IP and port
                 //String inputStream = socket.getInputStream().toString();
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(context, socket.isConnected() ? "Connected to server!":"Error while connecting",Toast.LENGTH_LONG).show();
+                        try {
+                            if(socket.isConnected() && socket != null) {
+                                outData = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket
+                                        .getOutputStream())), true); //create output stream to send data to server
+                                //outData.println("Connected to Server.");
+                            }
+                        }catch (IOException e){
+                            Log.e("remotedroid", "Error while creating OutWriter", e);
+                            Toast.makeText(context,"Error while connecting",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+                
                 while(socket.isConnected()) {
                     bMap = BitmapFactory.decodeStream(socket.getInputStream());
 
@@ -183,10 +269,6 @@ public class MainActivity extends AppCompatActivity {
                     Log.i("InputData", socket.getInputStream().toString());
                 }
 
-
-
-
-
             } catch (IOException e) {
                 Log.e("remotedroid", "Error while connecting", e);
                 result = false;
@@ -195,16 +277,15 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        protected void onPostExecute(Boolean result)
-        {
+        protected void onPostExecute(Boolean result) {
+            Log.d("OnPostExecute", "Called.");
             isConnected = result;
             Toast.makeText(context, isConnected ? "Connected to server!":"Error while connecting",Toast.LENGTH_LONG).show();
             try {
-                if(isConnected) {
+                if(socket.isConnected()) {
                     outData = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket
                             .getOutputStream())), true); //create output stream to send data to server
                     //outData.println("Connected to Server.");
-
                 }
             }catch (IOException e){
                 Log.e("remotedroid", "Error while creating OutWriter", e);
